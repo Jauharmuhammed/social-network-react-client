6045c5145f6b6d26b5fb36444f592f2269f48f82
Jauharmuhammed <jauharmuhammedk@gmail.com>
2023-01-04 11:48:46 +0530

    added emoji in comments

--- src/features/posts/components/Comments.jsx
+++ src/features/posts/components/Comments.jsx
@@ -1,41 +1,53 @@
-import {useAddCommentMutation, useGetCommentsByPostMutation} from "app/api/postApiSlice";
+import { useAddCommentMutation, useGetCommentsByPostMutation } from "app/api/postApiSlice";
 import classNames from "classnames";
 import Button from "components/Button";
 import ButtonSpinner from "components/ButtonSpinner";
-import React, {useEffect, useState} from "react";
-import {useRef} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import errorToast from "utils/toasts/errorToast";
-import { addComments, increasePostCommentCount, setComments, singlePostComments } from "../services/postSlice";
+import {
+    addComments,
+    increasePostCommentCount,
+    setComments,
+    singlePostComments,
+} from "../services/postSlice";
 import Comment from "./Comment";
+import EmojiPicker from "emoji-picker-react";
 
-const Comments = ({post}) => {
+const Comments = ({ post }) => {
     const user = useSelector((state) => state.auth.user);
-    const comments = useSelector(singlePostComments)
+    const comments = useSelector(singlePostComments);
     const [toggle, setToggle] = useState(false);
     const commentRef = useRef();
-    const [addComment, {isLoading}] = useAddCommentMutation();
+    const [addComment, { isLoading }] = useAddCommentMutation();
     const [getCommetnsByPost] = useGetCommentsByPostMutation();
     const [replyTo, setReplyTo] = useState(null);
-    const dispatch = useDispatch()
+
+    const [showEmoji, setShowEmoji] = useState(false);
+    const dispatch = useDispatch();
 
     async function handleSubmit() {
-        if (commentRef.current.value === "" || commentRef.current.value.trim() === `@${replyTo?.username}`) return;
-        const mention = `@${replyTo?.username}`
-        let data
-        if ( commentRef.current.value.includes(mention) === false ){
-            data ={
+        if (
+            commentRef.current.value === "" ||
+            commentRef.current.value.trim() === `@${replyTo?.username}`
+        )
+            return;
+        const mention = `@${replyTo?.username}`;
+        let data;
+        if (commentRef.current.value.includes(mention) === false) {
+            data = {
                 user: user.user_id,
                 post: post.id,
                 body: commentRef.current.value,
-            }
+            };
         } else {
             data = {
                 parent: replyTo ? replyTo.id : "",
                 user: user.user_id,
                 post: post.id,
                 body: commentRef.current.value.replace(`@${replyTo?.username} `, ""),
-            }
+            };
         }
         try {
             const response = await addComment(data).unwrap();
@@ -43,10 +55,10 @@ const Comments = ({post}) => {
             if (!replyTo) {
                 dispatch(addComments(response));
                 setToggle(true);
-                dispatch(increasePostCommentCount())
+                dispatch(increasePostCommentCount());
             } else {
                 fetchComments();
-                dispatch(increasePostCommentCount())
+                dispatch(increasePostCommentCount());
             }
 
             // set input as empty
@@ -61,7 +73,7 @@ const Comments = ({post}) => {
     async function fetchComments() {
         try {
             const response = await getCommetnsByPost(post?.id).unwrap();
-            dispatch(setComments(response))
+            dispatch(setComments(response));
         } catch (err) {
             console.log(err);
         }
@@ -80,6 +92,12 @@ const Comments = ({post}) => {
             setReplyTo(null);
         }
     }
+
+    function handleEmoji(emojiData) {
+        commentRef.current.value += emojiData?.emoji;
+        setShowEmoji(false);
+    }
+
 
     return (
         <div className="flex flex-col ">
@@ -104,7 +122,7 @@ const Comments = ({post}) => {
                 </span>
             </div>
 
-            {(toggle && comments.length > 0 ) && (
+            {toggle && comments.length > 0 && (
                 <ul className="list-none my-3 flex gap-3 flex-col">
                     {comments?.map((comment) => (
                         <Comment
@@ -117,14 +135,47 @@ const Comments = ({post}) => {
                 </ul>
             )}
             <div className="flex gap-2 items-center mb-10">
-                <img className="w-10 aspect-square rounded-full object-cover" src={user?.profile_pic} alt={user?.username} />
-                <input
-                    ref={commentRef}
-                    type="text"
-                    className="bg-transparent w-full rounded-3xl border py-2 px-3 outline-none"
-                    placeholder="Add a comment"
-                    onKeyDown={handleBackspace}
+                <img
+                    className="w-10 aspect-square rounded-full object-cover"
+                    src={user?.profile_pic}
+                    alt={user?.username}
                 />
+                <div className="w-full relative">
+                    <input
+                        ref={commentRef}
+                        type="text"
+                        className="bg-transparent w-full rounded-3xl border py-2 px-3 outline-none"
+                        placeholder="Add a comment"
+                        onKeyDown={handleBackspace}
+                        onClick={() => setShowEmoji(false)}
+                    />
+                    <span
+                        onClick={() => setShowEmoji((prev) => !prev)}
+                        className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1.5em"
+                            height="1.5em"
+                            preserveAspectRatio="xMidYMid meet"
+                            viewBox="0 0 64 64">
+                            <circle cx="32" cy="32" r="30" fill="#ffdd67" />
+                            <circle cx="22.3" cy="31.6" r="5" fill="#664e27" />
+                            <path
+                                fill="#917524"
+                                d="M51.2 27.5c-3.2-2.7-7.5-3.9-11.7-3.1c-.6.1-1.1-2-.4-2.2c4.8-.9 9.8.5 13.5 3.6c.6.5-1 2.1-1.4 1.7m-26.7-8.7c-4.2-.7-8.5.4-11.7 3.1c-.4.4-2-1.2-1.4-1.7c3.7-3.2 8.7-4.5 13.5-3.6c.7.2.2 2.3-.4 2.2"
+                            />
+                            <path
+                                fill="#664e27"
+                                d="M50.2 34.3c-1.7-3.5-4.4-5.3-7-5.3s-5.2 1.8-7 5.3c-.2.4.7 1 1.2.6c1.7-1.3 3.7-1.8 5.8-1.8s4.1.5 5.8 1.8c.4.3 1.3-.3 1.2-.6m-6.1 7.9c-6.9 3.6-16.4 2.9-19.1 2.9c-.7 0-1.2.3-1 .9c2 7 17 7 21.1-2.7c.5-1.1-.3-1.4-1-1.1"
+                            />
+                        </svg>
+                    </span>
+                    {showEmoji && (
+                        <span className="absolute bottom-12 right-0">
+                            <EmojiPicker onEmojiClick={handleEmoji} />
+                        </span>
+                    )}
+                </div>
 
                 <Button
                     disabled={isLoading}
