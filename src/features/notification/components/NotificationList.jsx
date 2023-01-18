9c24bc54cebe223b2fc3e429895d275757c8a050
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-01-18 12:21:51 +0530

    small screen optimisation for notification

--- src/features/notification/components/NotificationList.jsx
+++ src/features/notification/components/NotificationList.jsx
@@ -4,6 +4,7 @@ import TimeAgo from "components/TimeAgo";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { closeNotificationModal } from "../services/notificationModalSlice";
 import { setNotifications } from "../services/notifiicationSlice";
 
 const NotificationList = ({ connectionStatus, sendJsonMessage }) => {
@@ -41,7 +42,7 @@ const NotificationList = ({ connectionStatus, sendJsonMessage }) => {
             {notification_list.length > 0 ? notification_list?.map((notification) => (
                 <div key={notification.id} className="flex justify-between px-1.5 py-2 sm:hover:bg-stone-800 rounded-lg">
                     <div className="flex max-w-[82%]">
-                        <Link to={`/${notification.created_by_profile.username}`}>
+                        <Link to={`/${notification.created_by_profile.username}`} onClick={() => dispatch(closeNotificationModal())}>
                             <img
                                 className="w-10 aspect-square object-cover rounded-full"
                                 src={notification.created_by_profile.profile_pic}
@@ -50,7 +51,7 @@ const NotificationList = ({ connectionStatus, sendJsonMessage }) => {
                         </Link>
                         <div className="flex flex-col px-3 max-w-[85%]">
                             <p className="text-sm ">
-                                <Link to={`/${notification.created_by_profile.username}`}>
+                                <Link to={`/${notification.created_by_profile.username}`} onClick={() => dispatch(closeNotificationModal())}>
                                     {notification.created_by_profile.full_name}
                                 </Link>
                                 <span className="text-gray-400"> {notification.content}</span>
@@ -62,7 +63,7 @@ const NotificationList = ({ connectionStatus, sendJsonMessage }) => {
                         </div>
                     </div>
                     {notification.notification_type !== "follow"  && (
-                        <Link className="" to={`/post/${notification.post_details.id}`}>
+                        <Link className="" to={`/post/${notification.post_details.id}`} onClick={() => dispatch(closeNotificationModal())}>
                             <img
                                 className="w-12 aspect-square object-cover rounded"
                                 src={notification.post_details.image}
