d6dca64b63b0dfefc8b974d03307ee44e8e8f4dc
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-01-18 12:22:20 +0530

    url update

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,8 @@
 // local
-export const baseUrl = 'http://127.0.0.1:8000'
-export const wsBaseUrl = 'ws://127.0.0.1:8000'
+// export const baseUrl = 'http://127.0.0.1:8000'
+// export const wsBaseUrl = 'ws://127.0.0.1:8000'
 
 
 // production
-// export const baseUrl = 'https://showyourworkapi.jauharmuhammed.com'
-// export const wsBaseUrl = 'wss://showyourworkapi.jauharmuhammed.com'
+export const baseUrl = 'https://showyourworkapi.jauharmuhammed.com'
+export const wsBaseUrl = 'wss://showyourworkapi.jauharmuhammed.com'
